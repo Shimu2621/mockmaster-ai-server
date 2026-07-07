8f7f01e0558fec437b8774937771371386cb8307
Shimu Ziasmin <143342894+Shimu2621@users.noreply.github.com>
2026-07-06 22:00:03 -0400

    update user file

--- src/app/modules/userDashboard/userDashboard.controller.ts
+++ src/app/modules/userDashboard/userDashboard.controller.ts
@@ -3,6 +3,11 @@ import catchAsync from '@/app/utils/catchAsync';
 import { UserDashboardService } from './userDashboard.services';
 import { IUser } from '../users/users.interface';
 
+/**
+ * Retrieve dashboard data for the authenticated user.
+ * Get the authenticated user from the request.
+ */
+
 const getUserDashboardData = catchAsync(async (req: Request, res: Response) => {
   const user = req.user as IUser;
   const result = await UserDashboardService.getUserDashboardData(user.id);
